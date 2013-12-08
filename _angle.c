c36b444fcb982a8f2900b601e67c10779b837d20
Slavomir Hustaty <slavomir.hustaty@ixonos.com>
2013-12-08 16:15:28 +0100

    Eclipse code beautifier applied

--- _angle.c
+++ _angle.c
@@ -1,20 +1,20 @@
 // Specialty angular conversion functions
-double DegtoMOA(double deg){
-	return deg*60;
+double DegtoMOA(double deg) {
+	return deg * 60;
 }
-double DegtoRad(double deg){
-	return deg*3.14159265358979323846/180;
+double DegtoRad(double deg) {
+	return deg * 3.14159265358979323846 / 180;
 }
-double MOAtoDeg(double moa){
-	return moa/60;
+double MOAtoDeg(double moa) {
+	return moa / 60;
 }
-double MOAtoRad(double moa){
-	return moa/60*3.14159265358979323846/180;
+double MOAtoRad(double moa) {
+	return moa / 60 * 3.14159265358979323846 / 180;
 }
-double RadtoDeg(double rad){
-	return rad*180/3.14159265358979323846;
+double RadtoDeg(double rad) {
+	return rad * 180 / 3.14159265358979323846;
 }
-double RadtoMOA(double rad){
-	return rad*60*180/3.14159265358979323846;
+double RadtoMOA(double rad) {
+	return rad * 60 * 180 / 3.14159265358979323846;
 }
 
--- _windage.c
+++ _windage.c
@@ -1,19 +1,17 @@
-double Windage(double WindSpeed, double Vi, double xx, double t){
-	double Vw = WindSpeed*17.60; // Convert to inches per second.
-	return (Vw*(t-xx/Vi));
+double Windage(double WindSpeed, double Vi, double xx, double t) {
+	double Vw = WindSpeed * 17.60; // Convert to inches per second.
+	return (Vw * (t - xx / Vi));
 }
 
-
 // Headwind is positive at WindAngle=0
-double HeadWind(double WindSpeed, double WindAngle){
+double HeadWind(double WindSpeed, double WindAngle) {
 	double Wangle = DegtoRad(WindAngle);
-	return (cos(Wangle)*WindSpeed);
+	return (cos(Wangle) * WindSpeed);
 }
 
 // Positive is from Shooter's Right to Left (Wind from 90 degree)
-double CrossWind(double WindSpeed, double WindAngle){
+double CrossWind(double WindSpeed, double WindAngle) {
 	double Wangle = DegtoRad(WindAngle);
-	return (sin(Wangle)*WindSpeed);
+	return (sin(Wangle) * WindSpeed);
 }
-
 
